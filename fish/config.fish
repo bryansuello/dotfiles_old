6b8e849928054de2fc6af2c74c2bcffdb817c98a
suello.tech <suello.tech@gmail.com>
2022-07-24 01:09:24 +0800

    add coc extensions list and vs code settings.json

--- fish/config.fish
+++ fish/config.fish
@@ -7,10 +7,12 @@ set -U fish_greeting ""
  alias x="exit"
  alias v="nvim"
  alias tm="tmux"
- alias ls="ls -lah --color=auto"
- #alias ls="logo-ls -lah"
+ #alias ls="ls -lah --color=auto"
+ alias ls="logo-ls -lah"
  alias nps="npm start"
-
-
+ alias 1="cd /home/daddy/dev" #dev directory
+ alias 2="cd /mnt/c/Users/brys/" #c drive
+ alias 3="cd /mnt/x/" #x drive(ssd)
+ alias 4="cd ~/.config"
 
 end
